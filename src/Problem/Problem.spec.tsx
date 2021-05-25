d2fd1177cfbb2872cd492b9350c6fab854d3b831
Atul Anand <atulanand206@gmail.com>
2021-05-25 17:46:26 +0530

    Passed first test.

--- src/Problem/Problem.spec.tsx
+++ src/Problem/Problem.spec.tsx
@@ -2,9 +2,30 @@ import * as chai from 'chai';
 import { repeat } from './Problem';
 
 describe('Problem', function () {
+
+    const grid = [
+        [
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+        ],
+        [
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+        ],
+        [
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+            { top: 1, bottom: 0, left: 1, right: 4 },
+        ],
+    ]
+
     it('should call a', () => {
-        const a = repeat({ top: 1, bottom: 0, left: 1, right: 4 });
-        console.log(a);
-        console.log(a.blocks[1][1]);
+        const a = repeat();
+        chai.expect(a.blocks).deep.equal(grid);
     });
 });
--- src/Problem/Problem.tsx
+++ src/Problem/Problem.tsx
@@ -12,6 +12,7 @@ export interface Grid {
     blocks: Block[][];
 }
 
-export function repeat(val: Block): Grid {
-    return { blocks: [[val, val], [val, val, val]] }
+export function repeat(): Grid {
+    const val = { top: 1, bottom: 0, left: 1, right: 4 };
+    return { blocks: [[val, val, val, val], [val, val, val, val], [val, val, val, val]] }
 }
